726b4094632cbb962dfa449fbeb5d0fd56930f5f
Karolina Krzęcio <karolina.krzecio@gmail.com>
2022-03-27 20:35:17 +0200

    Grettings lista_zakupow.py

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -1,12 +1,2 @@
-shopping_list = {
 
-"piekarnia" : ["chleb", "pączek", "bułki"],
-"warzywniak": ["marchew", "seler", "rukola"]
-}
-print("Lista zakupionych  produktów")
-
-for keys, products in shopping_list.items():  
-      print(f"Idę do {keys.title()}, kupuję tu następujące rzeczy: {str(products).title()}")
-      products_1= len(shopping_list["piekarnia"])
-      products_2 = len(shopping_list["warzywniak"])
-print(f"Łącznie kupuję: {products_1 + products_2} produktów.")
+print("Pozdrawiam serdecznie, Mentorze! ;)")
