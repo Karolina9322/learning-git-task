54d57d997d1c16cf9ecdb8ab92a184d4be3b14bb
Karolina Krzęcio <karolina.krzecio@gmail.com>
2022-03-27 20:23:28 +0200

    Add products sum lista_zakupow.py

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -6,6 +6,7 @@ shopping_list = {
 print("Lista zakupów")
 
 for keys, products in shopping_list.items():  
-      print(f"Idę do {shops.title()}, kupuję tu następujące rzeczy: {str(products).title()}")
+      print(f"Idę do {keys.title()}, kupuję tu następujące rzeczy: {str(products).title()}")
       products_1= len(shopping_list["piekarnia"])
       products_2 = len(shopping_list["warzywniak"])
+print(f"W sumie kupuję: {products_1 + products_2} produktów.")
