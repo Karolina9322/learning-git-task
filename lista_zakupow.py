ca4d59131286f010f27b574405feeec65d81b794
Karolina Krzęcio <karolina.krzecio@gmail.com>
2022-03-27 20:22:19 +0200

    Add loop for lista_zakupow.py

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -3,3 +3,9 @@ shopping_list = {
 "piekarnia" : ["chleb", "pączek", "bułki"],
 "warzywniak": ["marchew", "seler", "rukola"]
 }
+print("Lista zakupów")
+
+for keys, products in shopping_list.items():  
+      print(f"Idę do {shops.title()}, kupuję tu następujące rzeczy: {str(products).title()}")
+      products_1= len(shopping_list["piekarnia"])
+      products_2 = len(shopping_list["warzywniak"])
